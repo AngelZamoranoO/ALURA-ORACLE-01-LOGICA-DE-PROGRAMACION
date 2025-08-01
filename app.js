44454f42c8c65ab93e47119fda447649ba92195c
Angel Zamorano <angel.zamorano@innova-ti.cl>
2025-08-01 02:20:42 -0400

    Mi primer comit

---
+++ app.js
@@ -0,0 +1,73 @@
+// // crea un alerta en la web
+// alert('Hola mundo');
+
+
+
+// crea un prompt en la web que solicitar cosas
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero");
+
+// esta realacionado a la consola de web
+console.log(numeroUsuario);
+
+// control de flujo
+if (numeroUsuario == numeroSecreto){
+    alert("Has acertado");
+} else {
+    alert('No has acertado')
+}
+
+/*
+Comprender estos conceptos no solo facilita el aprendizaje de nuevos lenguajes y tecnologías,
+ sino que también te capacita para generar soluciones innovadoras, depurar de manera eficiente y
+  mantener la calidad a lo largo del ciclo de vida del software.
+
+Por lo tanto, invertir tiempo en estos principios desde el principio es fundamental 
+para construir una carrera exitosa en el campo de la programación.
+*/
+
+//desafios
+
+// 1 - Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+// 2 - Declara una variable llamada nombre y asígnale el valor "Lua".
+let nombre = "Lua";
+
+// 3 - Crea una variable llamada edady asígnale el valor 25.
+let edad = 25;
+
+// 4 - Establece una variable numeroDeVentas y asígnale el valor 50.
+let numerodeVentas = 50;
+
+// 5 - Establece una variable saldoDisponible y asígnale el valor 1000.
+let saldoDisponible = 1000;
+
+// 6 - Muestra una alerta con el texto "¡Error! Completa todos los campos".
+alert("¡Error! Completa todos los campos");
+
+// 7 - Declara una variable llamada mensajeDeError y asígnale el valor
+//  "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+
+// 8- Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre
+let nombreDelUsuario = prompt("Escriba su nombre")
+
+// 9 - Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+let edadDelusuario = prompt("Escriba su edad");
+
+// 10 - Ahora, si la edad es mayor o igual a 18,
+//  muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".Si necesitas ayuda,
+//  las soluciones a las actividades estarán disponibles en la sección
+if (edadDelusuario >= 18){
+    alert("¡Puedes obtener tu licencia de conducir!")
+} else {
+    alert("Lo siento, no puedes obtener tu licencia de conducir")
+}
+
+// link de la documentacion de javascrip 
+// https://developer.mozilla.org/es/docs/Learn_web_development/Core/Scripting/What_is_JavaScript
+
+// link de la guia de javascript, como aprender el lenguaje mas popular del mundo
+// https://www.aluracursos.com/blog/guia-de-javascript
