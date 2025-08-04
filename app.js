27a2c0d56223c533b58d38bdee01751b5fc17ad5
Angel Zamorano <angel.zamorano@innova-ti.cl>
2025-08-04 01:18:40 -0400

    Se agrega mi logica en el juego

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+let numeroUsuario = 0;
+let intentos = 5;
+while(numeroSecreto != numeroUsuario || intentos != 0) {
 
-console.log(numeroUsuario);
+    let numeroUsuario = prompt(`Me indicas un numero del 1 al 10: tienes ${intentos} intentos`);
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    if (numeroUsuario == numeroSecreto) {
+        alert('Acertaste el numero');
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El numero secreto es menor que tu numero: '+ numeroUsuario);
+            
+        } else {
+            alert('El numero secreto es mayor que tu numero: '+ numeroUsuario);
+            
+        }
+        intentos --;
+    }
+    
 }
+if (intentos == 0) {
+    alert(`Has agotado los intentos, perdiste!!, el numero secreto es -> ${numeroSecreto}`);
+}
+ 
