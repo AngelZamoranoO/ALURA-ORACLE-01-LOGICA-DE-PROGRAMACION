12f0e411c449e8c4d09256f8d0da189a4617a247
Angel Zamorano <angel.zamorano@innova-ti.cl>
2025-08-04 01:53:55 -0400

    Termino de clase

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ if (intentos == 0) {
     alert(`Has agotado los intentos, perdiste!!, el numero secreto es -> ${numeroSecreto}`);
 }
  
+// desafios
+
+//1 Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+let contador = 1;
+while (contador <= 10) {
+    console.log(`Mostrando el numero ${contador}`);
+    contador ++;
+}
+
+//2 Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+let contadorDos = 10;
+while (contadorDos >= 0) {
+        console.log(`Mostrando el numero ${contadorDos}`);
+        contadorDos--;
+}
+//3 Crea un programa de cuenta regresiva. 
+// Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' 
+// en la consola del navegador.
+let pideNumero = prompt('Ingresa un numero');
+contador = 0;
+while (contador <= pideNumero) {
+    console.log(`Cuenta regresiva: ${pideNumero - contador}`);
+    contador++;
+}
+
+// 4 Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese 
+// número utilizando un bucle 'while' en la consola del navegador.
+let solicitaNumero = prompt('Ingresa en numero para la cuenta progresiva');
+contador = 0;
+while(contador <=solicitaNumero){
+    console.log(`Cuenta progresiva: ${contador}`);
+    contador++;
+}
