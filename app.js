9ffe59b1a38a223c12e2c3f5a59e148a440bde08
Angel Zamorano <angel.zamorano@innova-ti.cl>
2025-08-04 02:33:38 -0400

    Se actualiza el codigo

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 let numeroUsuario = 0;
-let intentos = 5;
+let intentos = 1;
+let maximoIntentos = 6
 while(numeroSecreto != numeroUsuario || intentos != 0) {
 
-    let numeroUsuario = prompt(`Me indicas un numero del 1 al 10: tienes ${intentos} intentos`);
+    let numeroUsuario = parseInt(prompt(`Me indicas un numero del 1 al 10:`));
 
     if (numeroUsuario == numeroSecreto) {
-        alert('Acertaste el numero');
+        alert(`Acertaste el numero es ${numeroUsuario}, lo hiciste en el ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
-            alert('El numero secreto es menor que tu numero: '+ numeroUsuario);
+            alert(`El numero secreto es menor que tu numero: ${numeroUsuario}, te quedan ${maximoIntentos - intentos} intentos`);
             
         } else {
-            alert('El numero secreto es mayor que tu numero: '+ numeroUsuario);
+            alert(`El numero secreto es mayor que tu numero: ${numeroUsuario}, te quedan ${maximoIntentos - intentos} intentos`);
             
         }
-        intentos --;
     }
-    
+    intentos++;
+    if(intentos >= maximoIntentos){
+        alert(`Llegaste al numero maximo de ${intentos} intentos, numero secreto es: ${numeroSecreto}`);
+        break;
+    }    
 }
-if (intentos == 0) {
-    alert(`Has agotado los intentos, perdiste!!, el numero secreto es -> ${numeroSecreto}`);
-}
- 
+alert('Fin del juego');
